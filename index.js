07ee8232a620da7130fb8d9f69c4287c31f7ebc4
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-19 18:29:58 +0600

    basic database connected with mongoDB and server

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send("hello, I am Okay");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ftixa.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected')
+  } finally {
+  }
+}
 
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("hello, I am Okay");
+});
 
-app.listen(port, ()=>{
-    console.log("server running")
+app.listen(port, () => {
+  console.log("server running");
 });
